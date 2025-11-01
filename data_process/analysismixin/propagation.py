988c1a453ccce5b042a3f3737e54aaa1b5621969
Qixian-Wang <wqx1377823969@outlook.com>
2025-10-31 19:27:04 -0500

    modify propagation plot

--- data_process/analysismixin/propagation.py
+++ data_process/analysismixin/propagation.py
@@ -7,17 +7,18 @@ from config_file import configs
 
 class PropagationAnalysisMixin(TruncateDataMixIn):
     def compute_propagation(self, config):
-        data = self.firing_rate_result["spike_rates"]
-        
-        truncated_data = self.truncate_data(data, 0, configs.stim_interval)
-
+        analysis_duration = config["max_time_delay"]
         rate_threshold = config["rate_threshold"]
+
+        data = self.firing_rate_result["spike_rates"]
+        truncated_data = self.truncate_data(data, 0, analysis_duration)
 
         for pattern_idx in range(configs.num_patterns):
             data_for_pattern = [truncated_data[i] for i in range(len(truncated_data)) if i % configs.num_patterns == pattern_idx]
 
             for data_idx, data_segment in enumerate(data_for_pattern):
                 time_arrival_list = []
+                max_firing_rate_list = []
                 for channel_data in data_segment:
                     mask = channel_data > rate_threshold
                     if np.any(mask):
@@ -26,7 +27,8 @@ class PropagationAnalysisMixin(TruncateDataMixIn):
                     else:
                         time_delay = np.nan
 
-                    time_arrival_list.append(time_delay)
+                    time_arrival_list.append(time_delay if time_delay < analysis_duration else np.nan)
+                    max_firing_rate_list.append(np.max(channel_data))
 
-                plot_propagation_map(self.figure_save_path, self.session_name, data_idx, pattern_idx, time_arrival_list)
+                plot_propagation_map(self.figure_save_path, self.session_name, data_idx, pattern_idx, time_arrival_list, max_firing_rate_list)
 
