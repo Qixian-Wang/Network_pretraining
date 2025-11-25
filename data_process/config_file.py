52c82c3e30de3922caf14b9180ec8796fd80dab8
Qixian-Wang <wqx1377823969@outlook.com>
2025-11-25 14:08:16 -0600

    clean up run.py fix psth computing

--- data_process/config_file.py
+++ data_process/config_file.py
@@ -9,7 +9,7 @@ class Configs(BaseModel):
     # mea_type: str = "512_long"
 
     # Stimulation parameters
-    excluded_channels: list[str] = ['A-000', 'A-003', 'A-028', 'A-029', 'A-030', 'B-010', 'B-019', 'B-022', 'B-031', 'C-003', 'C-006', 'C-007', 'C-020', 'C-022', 'D-016', 'D-017', 'D-018', 'D-019', 'D-020', 'D-021', 'D-022', 'D-023', 'D-025', 'D-026', 'D-030', 'A-015']
+    excluded_channels: list[str] = ['A-031', 'B-002', 'B-018', 'C-031', 'D-016', 'D-017', 'D-018']
 
     reading_channels: list[str] = [
                         'A-000', 'A-001', 'A-002', 'A-003', 'A-004', 'A-005', 'A-006', 'A-007', 
@@ -20,20 +20,21 @@ class Configs(BaseModel):
                         'D-008', 'D-009', 'D-010', 'D-011', 'D-012', 'D-013', 'D-014', 'D-015', 
                         'D-016', 'D-017', 'D-018', 'D-019', 'D-020', 'D-021', 'D-022', 'D-023', 
                         'D-024', 'D-025', 'D-026', 'D-027', 'D-028', 'D-029', 'D-030', 'D-031',
-                        # 'B-000', 'B-001', 'B-002', 'B-003', 'B-004', 'B-005', 'B-006', 'B-007', 
-                        # 'B-008', 'B-009', 'B-010', 'B-011', 'B-012', 'B-013', 'B-014', 'B-015', 
-                        # 'B-016', 'B-017', 'B-018', 'B-019', 'B-020', 'B-021', 'B-022', 'B-023', 
-                        # 'B-024', 'B-025', 'B-026', 'B-027', 'B-028', 'B-029', 'B-030', 'B-031',
-                        # 'C-000', 'C-001', 'C-002', 'C-003', 'C-004', 'C-005', 'C-006', 'C-007', 
-                        # 'C-008', 'C-009', 'C-010', 'C-011', 'C-012', 'C-013', 'C-014', 'C-015', 
-                        # 'C-016', 'C-017', 'C-018', 'C-019', 'C-020', 'C-021', 'C-022', 'C-023', 
-                        # 'C-024', 'C-025', 'C-026', 'C-027', 'C-028', 'C-029', 'C-030', 'C-031'
+                        'B-000', 'B-001', 'B-002', 'B-003', 'B-004', 'B-005', 'B-006', 'B-007', 
+                        'B-008', 'B-009', 'B-010', 'B-011', 'B-012', 'B-013', 'B-014', 'B-015', 
+                        'B-016', 'B-017', 'B-018', 'B-019', 'B-020', 'B-021', 'B-022', 'B-023', 
+                        'B-024', 'B-025', 'B-026', 'B-027', 'B-028', 'B-029', 'B-030', 'B-031',
+                        'C-000', 'C-001', 'C-002', 'C-003', 'C-004', 'C-005', 'C-006', 'C-007', 
+                        'C-008', 'C-009', 'C-010', 'C-011', 'C-012', 'C-013', 'C-014', 'C-015', 
+                        'C-016', 'C-017', 'C-018', 'C-019', 'C-020', 'C-021', 'C-022', 'C-023', 
+                        'C-024', 'C-025', 'C-026', 'C-027', 'C-028', 'C-029', 'C-030', 'C-031'
                         ]
  
-    session_names: list[str] = ["train0", "train1", "train2", "train3", "train4", "train5"]
+    session_names: list[str] = ["spontaneous1", "spontaneous2", "spontaneous3", "train0", "train1", "train2", "train3", "train4", "train5", "train6"]
 
     # Pattern parameters
-    num_patterns: int = 6
+    num_pretrain_patterns: int = 4
+    num_train_patterns: int = 6
     
     component: dict[int, list[int]] = {
         0: [0, 1, 2, 3, 8, 9, 10, 11, 16, 17, 18, 19, 24, 25, 26, 27],
@@ -42,7 +43,7 @@ class Configs(BaseModel):
         3: [100, 101, 102, 103, 108, 109, 110, 111, 116, 117, 118, 119, 124, 125, 126, 127],
     }
 
-    pattern_dict: dict[int, list[int]] = {
+    train_pattern_dict: dict[int, list[int]] = {
         0: component[0]+component[1],
         1: component[0]+component[2],
         2: component[0]+component[3],
@@ -50,14 +51,20 @@ class Configs(BaseModel):
         4: component[1]+component[3],
         5: component[2]+component[3],
     }
-
+    
+    pretrain_pattern_dict: dict[int, list[int]] = {
+        0: component[0],
+        1: component[1],
+        2: component[2],
+        3: component[3],
+    }
+    
     stim_interval: float = 4
 
     # File paths
     mea_yaml_path: str = f"/Users/aia/Desktop/cppcode/Intan-RHX/tcp/data_process/mea_topology/{mea_type}.yaml"
-    data_path: str = "/Users/aia/Desktop/data/data_Batch2/9_30_data/spike_train"
+    data_path: str = "/Users/aia/Desktop/data/11_22_data_sample4/spike_train"
     figure_save_path: str = "figures"
-
 
     
 configs = Configs()
--- data_process/pipeline/run.py
+++ data_process/pipeline/run.py
@@ -12,6 +12,8 @@ from visualization.correlation_matrix import plot_correlation_matrix
 from visualization.dimension_reduction import plot_silhouette_score
 from visualization.correlation_matrix import plot_correlation_matrix
 
+from config_file import configs
+
 
 class Pipeline(DimensionReductionMixIn, MovingAverageFiringRateMixIn, PsthAnalysisMixin, PropagationAnalysisMixin, OrganizeChannelsMixIn):
 
@@ -27,35 +29,6 @@ class Pipeline(DimensionReductionMixIn, MovingAverageFiringRateMixIn, PsthAnalys
 
         os.makedirs(self.figure_save_path, exist_ok=True)
 
-        self.compute_firing_rate_flag = False
-        self.dim_reduction_flag = False
-        self.psth_flag = False
-        self.correlation_matrix_flag = False
-        self.propagation_map_flag = False
-
-        if self.cfg["process"]["firing_rate"]["method"] is not None:
-            self.compute_firing_rate_flag = True
-            self.firing_rate_config = self.cfg["process"]["firing_rate"]
-
-        if self.cfg["process"]["dimension_reduction"]["execute"]:
-            self.dim_reduction_flag = True
-            self.dim_reduction_config = self.cfg["process"]["dimension_reduction"]
-            self.silhouette_score_tsne = []
-            self.silhouette_score_pca = []
-
-        if self.cfg["process"]["psth"]["execute"]:
-            self.psth_flag = True
-            self.psth_config = self.cfg["process"]["psth"]
-
-        if self.cfg["process"]["additional_plotting"]["correlation_matrix"]["execute"]:
-            self.correlation_matrix_flag = True
-            self.duration = self.correlation_matrix_config["duration"]
-            self.correlation_matrix_config = self.cfg["process"]["additional_plotting"]["correlation_matrix"]
-
-        if self.cfg["process"]["additional_plotting"]["propagation_map"]["execute"]:
-            self.propagation_map_flag = True
-            self.propagation_map_config = self.cfg["process"]["additional_plotting"]["propagation_map"]
-
 
     def run(self):
         print("Loading data...")
@@ -68,22 +41,32 @@ class Pipeline(DimensionReductionMixIn, MovingAverageFiringRateMixIn, PsthAnalys
             self.spike_train = dataset.spike_train
             self.stimulation_init_time = dataset.stimulation_init_time
 
-            if self.compute_firing_rate_flag:
-                self.compute_firing_rate(self.firing_rate_config)
+            if self.session_name.startswith("train"):
+                self.num_patterns = configs.num_train_patterns
+                self.pattern_dict = configs.train_pattern_dict
+            else:
+                self.num_patterns = configs.num_pretrain_patterns
+                self.pattern_dict = configs.pretrain_pattern_dict
 
-            if self.dim_reduction_flag and self.session_name.startswith("train"):
-                self.compute_dimension_reduction(self.dim_reduction_config)
-            
-            if self.psth_flag and self.session_name.startswith("train"):
-                self.compute_psth(self.psth_config)
 
-            if self.correlation_matrix_flag and self.session_name.startswith("spontaneous"):
-                plot_correlation_matrix(self.figure_save_path, self.session_name, self.firing_rate_result, self.step_size, self.num_channels, self.duration)
+            self.compute_firing_rate(self.self.cfg["process"]["firing_rate"])
 
-            if self.propagation_map_flag and self.session_name.startswith("train"):
-                self.compute_propagation(self.propagation_map_config)
-        
-        
+            if "dimension_reduction" in self.cfg["optional_processes"]:
+                self.silhouette_score_tsne = []
+                self.silhouette_score_pca = []
+                self.compute_dimension_reduction(self.cfg["optional_processes"]["dimension_reduction"])
+
+            if "psth" in self.cfg["optional_processes"] and self.session_name.startswith("train") or self.session_name.startswith("pretrain"):
+                self.compute_psth(self.cfg["optional_processes"]["psth"])
+
+            if "correlation_matrix" in self.cfg["optional_processes"] and self.session_name.startswith("spontaneous"):
+                duration = self.cfg["optional_processes"]["correlation_matrix"]["duration"]
+                plot_correlation_matrix(self.figure_save_path, self.session_name, self.firing_rate_result, self.step_size, self.num_channels, duration)
+
+            if "propagation_map" in self.cfg["optional_processes"] and self.session_name.startswith("train"):\
+                self.compute_propagation(self.cfg["optional_processes"]["propagation_map"])
+
+
         if self.dim_reduction_flag:
             plot_silhouette_score(self.figure_save_path, self.silhouette_score_tsne, self.silhouette_score_pca)
 
